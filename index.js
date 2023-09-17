140723bc2233f5e1840704c615b843709e05f573
Gustavo Pinto <gustavvupinto@gmail.com>
2023-09-17 14:58:59 -0300

    nota fiscal

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var saldo = 0
 var laçoDeRepetição = true
 while(laçoDeRepetição){
     var resposta = window.prompt(`Sistema de peças / 1-Entrada / 2-Saida / 3-Sair                         | saldo: ${saldo}`)
-  
+    //document.write(`Saldo: ${saldo} <br>`)
     switch(resposta){
         case "1":
             var registro = Number.parseInt(prompt("Quantas peças serão registradas?"))
-            saldo = saldo + registro
+                saldo = saldo + registro
+                document.write(`Saldo: +${registro}<br>`)
             break;
         case "2":
             var vendas = prompt("Quantas peças serão vendidas?")
@@ -15,6 +16,7 @@ while(laçoDeRepetição){
                 alert("Numero do estoque insuficiente")
             }else{
                 saldo = saldo - vendas
+                document.write(`Saldo: -${vendas}<br>`)
             }
             break;
         case "3":
@@ -24,5 +26,8 @@ while(laçoDeRepetição){
         default:
             alert("algo deu errado")
             break;
+            
     }
+   
 }
+document.write(`Saldo final: ${saldo}`)
